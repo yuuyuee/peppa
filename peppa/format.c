7ab96b5d832b61e51f2e0ed86244ba57c95c4589
Yue Yu <yuyue2200@hotmail.com>
2023-08-01 18:59:29 +0800

    update

--- peppa/format.c
+++ peppa/format.c
@@ -4,15 +4,15 @@
 
 #include <stdio.h>
 
-size_t Pe_fmt(char* buf, size_t size, const char* fmt, ...) {
+size_t Pe_format(char* buf, size_t size, const char* fmt, ...) {
   va_list ap;
   va_start(ap, fmt);
-  int ret = Pe_fmtv(buf, size, fmt, ap);
+  int ret = Pe_formatv(buf, size, fmt, ap);
   va_end(ap);
   return ret;
 }
 
-size_t Pe_fmtv(char* buf, size_t size, const char* fmt, va_list ap) {
+size_t Pe_formatv(char* buf, size_t size, const char* fmt, va_list ap) {
   int ret = vsnprintf(buf, size, fmt, ap);
   if (ret < 0) {
     buf[0] = '\0';
